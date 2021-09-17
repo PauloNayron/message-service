62c908b09929299bdb1201fc84f9d93abaf2e0f4
PauloNayron <ext_panayron@mercadolivre.com>
2021-09-17 01:40:17 -0300

    config: implement open api

--- src/main/java/com/lb/messageservice/app/controller/ReportController.java
+++ src/main/java/com/lb/messageservice/app/controller/ReportController.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestControllerAdvice
 @RequiredArgsConstructor
 @RequestMapping("report")
 public class ReportController implements Loggable {
--- src/main/java/com/lb/messageservice/app/controller/dto/ReportDTO.java
+++ src/main/java/com/lb/messageservice/app/controller/dto/ReportDTO.java
@@ -4,12 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lb.messageservice.app.commons.ChannelType;
 import com.lb.messageservice.domain.entity.enums.Status;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public record ReportDTO(
-        Long recipient,
-        @JsonProperty("send_date") LocalDateTime sendDate,
-        String message,
-        ChannelType channel,
+        @NotBlank @JsonProperty("recipient") Long recipient,
+        @NotBlank @JsonProperty("send_date") LocalDateTime sendDate,
+        @NotBlank @JsonProperty("message") String message,
+        @NotBlank @JsonProperty("channel") ChannelType channel,
         Status status
 ) { }
